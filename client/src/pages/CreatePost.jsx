692bae313034288f4f0fa3d4945299ffe654de42
BaoNguyen09 <nttbvlogs@gmail.com>
2024-10-28 22:59:56 -0700

    client/src/pages/CreatePost.jsx: 	- make handleChange and handleSurpriseMe functions work
    Create server folder for backend
    
    server/index.js:
    	- main file to run the backend: run server and connect to db
    server/mongodb/connect.js:
    	- this file connect with mongodb atlas
    server/package.json:
    	- include start with nodemon to run the server
    	- use module type in node.js
    	- install cloudinary, cors, dotenv, express, mongoose, nodemon, openai packages

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,10 +17,12 @@ const CreatePost = () => {
 
   const handleSubmit = () => {};
   const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
 
   }
   const handleSurpriseMe = () => {
-
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt});
   }
 
   const generateImage = () => {}
