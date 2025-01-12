d06ee8c8b3898f6246c828ef6471072ef9fa5ddf
BaoNguyen09 <nttbvlogs@gmail.com>
2025-01-11 19:48:47 -0700

    feat: add clien-side text search feature

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Loader, Card, FormField } from '../components';
 
 const RenderCards = ({ data, title }) => {
@@ -18,6 +18,8 @@ const Home = () => {
   const [allPosts, setAllPosts] = useState(null);
 
   const [searchText, setSearchText] = useState('');
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -42,6 +44,18 @@ const Home = () => {
     fetchPosts();
   }, []);
 
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+    setSearchTimeout(
+        setTimeout(() => {
+            const searchedResults = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || 
+                item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+            setSearchedResults(searchedResults);
+        }, 500)
+    );
+  }
+
   return (
     <section className='max-w-7xl mx-auto'>
         <div>
@@ -54,7 +68,14 @@ const Home = () => {
         </div>
 
         <div className='mt-16'>
-            <FormField />
+            <FormField 
+                LabelName="Search posts"
+                type="text"
+                name="text"
+                placeholder="Search posts"
+                value={searchText}
+                handleChange={handleSearchChange}
+            />
         </div>
 
         <div className='mt-10'>
@@ -72,7 +93,7 @@ const Home = () => {
                 <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
                     {searchText ? (
                         <RenderCards 
-                            data={[]}
+                            data={searchedResults}
                             title="No search results found"
                         />
                     ) : (
